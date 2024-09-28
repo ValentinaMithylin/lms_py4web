e834e23f8ce7d5cf633246d789cae8f209dcf69b
Valentina Mithylin <85881813+ValentinaMithylin@users.noreply.github.com>
2024-09-28 15:08:09 +0600

    course/progress test view

---
+++ controllers/progress.py
@@ -0,0 +1,15 @@
+from py4web import action
+
+# from ..models.connect_db import db
+
+@action('home/progress')
+@action.uses('progress.html')
+def progress():
+    # user_name = ''
+    # get_user_name_sql = f"SELECT name FROM users WHERE user_id = '007' LIMIT 0, 1;"
+    # get_user_name = db.executesql(get_user_name_sql, as_dict = True)
+
+    # if get_user_name:
+    #     user_name = str(get_user_name[0]['name']).split(' ')[0]
+
+    return locals()
