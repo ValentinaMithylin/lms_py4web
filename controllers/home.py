4be02fdc80f234ac0c6e8bb84f648c52877d2d66
Valentina Mithylin <85881813+ValentinaMithylin@users.noreply.github.com>
2024-09-28 13:22:50 +0600

    dashboard changes

--- controllers/home.py
+++ controllers/home.py
@@ -1,7 +1,15 @@
 from py4web import action
 
+from ..models.connect_db import db
+
 @action('home/dashboard')
 @action.uses('dashboard.html')
 def dashboard():
-    # return "You have been redirected to a new page!"
+    user_name = ''
+    get_user_name_sql = f"SELECT name FROM users WHERE user_id = '007' LIMIT 0, 1;"
+    get_user_name = db.executesql(get_user_name_sql, as_dict = True)
+
+    if get_user_name:
+        user_name = str(get_user_name[0]['name']).split(' ')[0]
+
     return locals()
