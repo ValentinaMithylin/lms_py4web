d383f65156cc2af52522ef31768a0e8f8ef8210d
Valentina Mithylin <85881813+ValentinaMithylin@users.noreply.github.com>
2024-09-28 12:12:55 +0600

    created dashboard

--- controllers/home.py
+++ controllers/home.py
@@ -1,5 +1,7 @@
 from py4web import action
 
 @action('home/dashboard')
+@action.uses('dashboard.html')
 def dashboard():
-    return "You have been redirected to a new page!"
+    # return "You have been redirected to a new page!"
+    return locals()
